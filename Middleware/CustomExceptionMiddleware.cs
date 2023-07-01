575eb967a72ea78ec838429950177ac9ebdcffbf
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-07-01 16:01:27 +0300

    middleware is added

---
+++ Middleware/CustomExceptionMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore.Middleware
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            string message = "";
+        }
+    }
+}
