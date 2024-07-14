032ded65db411c0e2317c32dcd6f3531ba3880df
fatmanurkaramann <nur.karaman1@std.yildiz.edu.tr>
2024-07-14 16:55:57 +0300

    name changed

--- obj/Debug/net6.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net6.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57bbfb4380498bf03380f09ca0103dcc89a1ee16")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
